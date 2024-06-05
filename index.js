cc83fead6d77657337cafb8ff9908b5a741a6f3a
mukhammadaliyev1 <muhammadnurmamadaliyev335@gmail.com>
2024-06-05 17:04:53 +0500

    a

---
+++ index.js
@@ -0,0 +1,480 @@
+//  1-misol
+
+
+// let a = +prompt('a');
+// let b = '';
+// if(a>0){
+  
+//    console.log (b = 'true');
+// }else{
+//    console.log (b = 'false');
+// }
+
+
+
+
+// //  2-misol
+
+// let a = +prompt('a');
+// let b =  '';
+// if(a%2==1){
+//     console.log(b = 'toq');
+// }else{
+//     console.log(b = 'juft');
+// }
+
+
+//  3-misol
+
+
+// let a = +prompt('a');
+// let b =  '';
+// if(a%2==0){
+//     console.log(b = 'true');
+// }else{
+//     console.log(b = 'false');
+// }
+
+
+//  4-misol
+
+
+// let A = +prompt('A');
+
+// let B = +prompt('B');
+// let c = '';
+// if ( A > 2 && B >=3){
+//     console.log(c = 'true');
+// }else{
+//     console.log(c = 'false');
+// }
+
+
+
+//  5-misol
+
+
+
+
+// let A = +prompt('A');
+
+// let B = +prompt('B');
+// let c = '';
+// if ( A >= 0 && B < -2){
+//     console.log(c = 'true');
+// }else{
+//     console.log(c = 'false');
+// }
+
+
+
+//  6-misol
+
+
+
+// let A = +prompt('A');
+
+// let B = +prompt('B');
+
+//  let C = +prompt('C');
+// let c = '';
+// if ( A <= B && B <=C){
+//     console.log(c = 'true');
+// }else{
+//     console.log(c = 'false');
+// }
+
+
+// 7 - misol
+
+
+
+// let A = +prompt('A');
+// let B = +prompt('B');
+// let C = +prompt('C');
+// let c = '';
+// if( A>B && B > C){
+//     console.log(c= 'true');
+// }else{
+//     console.log(c= 'false'
+//     );
+// }
+
+
+
+// 8-misol
+
+// let A = +prompt('A');
+// let B = +prompt('B');
+// let c = '';
+// if( A%2==1 && B%2==1){
+//     console.log(c= 'true');
+// }else{
+//     console.log(c= 'false'
+//     );
+// }
+
+
+
+// 9-misol 
+
+
+
+// let A = +prompt('A');
+// let B = +prompt('B');
+// let c = '';
+// if( A%2==1 || B%2==1){
+//     console.log(c= 'true');
+// }else{
+//     console.log(c= 'false'
+//     );
+// }
+
+
+// 10-misol
+
+
+
+
+// let A = +prompt('A');
+// let B = +prompt('B');
+// let c = '';
+// if( A%2==1 || B%2==0){
+//     console.log(c= 'true');
+// }else{
+//     console.log(c= 'false'
+//     );
+// }
+
+
+// //  11 - misol
+
+// let A =  +prompt('A');
+// let B =  +prompt('B');
+// let c = '';
+//  if(A%2==0 && B%2==0  ){
+//     console.log(c= 'true')
+// }else if (A%2==1 && B%2==1){
+//     console.log(c='true');
+// }else {
+//     console.log(c = 'false')
+// }
+
+
+
+// // 12 -misol
+
+// let A = +prompt('A');
+// let B = +prompt('B');
+// let C = +prompt('C');
+// let c = '';
+
+// if(A>0 && B > 0 && C >0){
+//     console.log( c= 'true');
+// }
+// else {
+//         console.log(c = 'false')
+//     }
+    
+
+
+
+
+// 13 -misol
+
+// let A = +prompt('A');
+// let B = +prompt('B');
+// let C = +prompt('C');
+// let c = '';
+
+// if(A>0 || B > 0 || C >0){
+//     console.log( c= 'true');
+// }
+// else {
+//         console.log(c = 'false')
+//     }
+   
+
+// // 14 -misol
+
+
+
+// let A = +prompt('A');
+// let B = +prompt('B');
+// let C = +prompt('C');
+// let c = '';
+// if(A>0 && B<0 && C < 0 ){
+//     console.log(c = 'true');
+// }
+// if(C>0 &&  A<0 && B>0){console.log(c='true')}
+// else if (A<0 && B>0 && C<0   ){console.log(c='true');}
+// else{
+//     console.log(c = 'false');
+// }
+
+
+
+
+// 15-misol
+
+
+
+
+
+
+
+// 
+
+
+
+
+// // 16-misol
+
+// let a = +prompt('sonni kiriting');
+// let b = '';
+// if(a>=10 && a%2==0){
+//     console.log(b = 'true');
+// }else{
+//     console.log( b = 'false');
+// }
+
+
+
+
+// 17-misol
+
+
+
+// let a = +prompt('sonni kirit');
+// let b = a;
+// if(a>0){
+//     b= a+1;
+//     console.log( b)
+
+// }
+// else{
+//     console.log(b)
+// }
+
+
+
+// 18-misol
+
+
+
+// let a = +prompt('sonni kirit');
+// let b = a;
+// if(a>0){
+//     b= a+1;
+//     console.log( b)
+
+// }
+// else{
+//     b= a-2;
+//     console.log(b)
+// }
+
+
+
+
+// 19-misol
+
+
+
+// let a = +prompt('sonni kirit');
+// let b = a;
+// if(a>0){
+//     b= a+1;
+//     console.log( b)
+
+// }else if(
+//     a==0
+// ){
+//     b=10;
+//     console.log(b)
+// }
+
+
+// else{
+//     b= a-2;
+//     console.log(b)
+// }
+
+
+
+// 20 -misol
+// let a = +prompt(' a ni kirit');
+// let b = +prompt(' b ni kirit');
+// let c = +prompt(' c ni kirit');
+// let d = '';
+
+// if (a>0){
+//     d++;
+// }
+// if (b>0){
+//     d++;
+// }
+// if (c>0){
+//     d ++;
+// }
+
+// console.log("musbat sonlar soni :" +d);
+
+
+
+
+
+
+
+// 21 -misol
+
+
+
+// let a = +prompt(' a ni kirit');
+// let b = +prompt(' b ni kirit');
+// let c = +prompt(' c ni kirit');
+// let d = '';
+// let e = '';
+// if (a>0){
+//     d++;
+// }else{
+//     e++;
+// }
+// if (b>0){
+//     d++;
+// }else{
+//     e++;
+// }
+// if (c>0){
+//     d ++;
+// }
+// else{
+//     e++;
+// }
+// console.log("musbat sonlar soni :" +d);
+// console.log("manfiy sonlar soni :" +e);
+
+
+
+
+
+// 22-misol
+
+
+
+// let  a = +prompt('birinchi sonni kiriting');
+// let b = +prompt('ikkinchi sonni kiriting');
+// let c = '';
+// if (a>b){
+//     c= 'birinchi soon katta'
+
+// }
+// else{
+//     c= 'ikkinchi soon katta'    
+// }
+// console.log(c);
+
+
+
+//  23-misol
+
+
+// let  a = +prompt('birinchi sonni kiriting');
+// let b = +prompt('ikkinchi sonni kiriting');
+// let c = '';
+// if( a>b){
+//     console.log(b)
+// }else{
+//     console.log(a)
+// }
+
+
+
+// //  24-,misol
+
+// let  a = +prompt('birinchi sonni kiriting');
+// let b = +prompt('ikkinchi sonni kiriting');
+// let c = '';
+// if(a>b){
+//    console.log(a)
+//    console.log(b) 
+// }
+// else{
+//     console.log(b)
+//     console.log(a) 
+// }
+
+
+
+
+// 25 -misol
+
+
+// let a = +prompt('birinchi sonni kiriting');
+// let b = +prompt('ikkinchi sonni kiriting');
+// let c = '';
+
+// if (a<b){
+//     console.log(a)
+//     console.log(b)
+// }else{
+//     b=b+a;
+// console.log(b)
+// console.log(a)
+// }
+
+
+
+
+
+// // // 26-misol
+// let A = +prompt('A');
+// let B = +prompt('B');
+// let c = '';   
+// if ( A != B){
+//     c= A+B;
+// }
+// else if (A==B){
+//     c=0;
+// }
+// else{
+//     console.log(A)
+//     console.log(B)
+// }
+// console.log(c);
+
+
+
+// 27-misol
+
+// let a = +prompt('a');
+// let b = +prompt('b');
+// let c = +prompt('c');
+// if(a<b && a<c){
+//     console.log(a)
+// }
+// else if(b<a && b<c){
+//     console.log(b)
+// }
+// else{
+//     console.log(c)
+// }
+
+
+
+
+// 28-misol
+
+
+
+// let a = +prompt('a');
+//  let b = +prompt('b');
+//  let c = +prompt('c');
+
+//  if (a>b && b>c){
+//      console.log(b)
+//  }else if(a>c && c>b){
+//      console.log(c)
+//  }else{
+//      console.log(a)
+// 
